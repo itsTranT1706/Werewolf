fe3c663f6059ffbd1ed1070f0f9b1933b0e0c079
Quan Truong <158136735+tmq3007@users.noreply.github.com>
2025-12-30 16:20:47 +0700

    feat(profile): update API versioning for user endpoints

--- services/profile/src/main/java/com/profile/controller/UserController.java
+++ services/profile/src/main/java/com/profile/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
